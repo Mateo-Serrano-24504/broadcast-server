d8b84eb457b80217bd2dd195412976eaea2916dd
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-11-16 17:46:36 +0000

    style: ran prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,14 @@ import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    eslintConfigPrettier,
-    {
-        rules: {
-            '@typescript-eslint/no-unused-vars': 'warn',
-            '@typescript-eslint/no-explicit-any': 'warn',
-            'no-console': 'off'
-        }
-    }
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': 'off',
+    },
+  }
 );
