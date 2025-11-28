978d389cb1e0e8f994ff370792b77937cb6168ae
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-11-28 23:30:40 +0000

    feat(user): added a UserData interface and updated User
    Moved basic fields to a UserData interface and updated User to be constructed with an id. Also added
    a method to create a User from an id and a UserData
    
    BREAKING CHANGE: User must now be created with an id

--- src/features/user/user.entity.ts
+++ src/features/user/user.entity.ts
@@ -1,11 +1,18 @@
-import type { UserRole } from './user.types';
+import type { UserRole, UserData } from './user.types';
 
 export class User {
   constructor(
+    private _id: number,
     private _username: string,
     private _password: string,
     private _role: UserRole
   ) {}
+  fromUserData(id: number, userData: UserData): User {
+    return new User(id, userData.username, userData.password, userData.role);
+  }
+  get id() {
+    return this._id;
+  }
   get username() {
     return this._username;
   }
--- src/features/user/user.types.ts
+++ src/features/user/user.types.ts
@@ -4,3 +4,9 @@ export const UserRole = {
 } as const;
 
 export type UserRole = (typeof UserRole)[keyof typeof UserRole];
+
+export interface UserData {
+  username: string;
+  password: string;
+  role: UserRole;
+}
