f061f34699cdca784788c9f8d03c0c87912d65b6
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-12-03 19:42:02 +0000

    feat(user): added a loginUser method to the UserService class

--- src/features/user/user.service.ts
+++ src/features/user/user.service.ts
@@ -1,14 +1,23 @@
-import { User } from './user.entity';
-import { UserRepository } from './user.repository';
-import { UserData } from './user.types';
+import { User, UserRepository, UserData } from '../user';
+import { AuthService, InvalidCredentialsError, JwtPayload } from '../auth';
 
 export class UserService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private authService: AuthService
+  ) {}
 
   async createUser(userData: UserData): Promise<number> {
     return this.userRepository.save(userData);
   }
   async removeUser(userId: number): Promise<void | User> {
     return this.userRepository.remove(userId);
+  }
+  async loginUser(jwtPayload: JwtPayload): Promise<string> {
+    if (await this.userRepository.verifyUserById(jwtPayload.id)) {
+      return this.authService.generateAccessToken(jwtPayload);
+    } else {
+      throw new InvalidCredentialsError();
+    }
   }
 }
