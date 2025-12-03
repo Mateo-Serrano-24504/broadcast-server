d617a3172789b6fa93bd7f4b99a2ce14a9910b24
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-12-03 19:17:27 +0000

    feat(auth): added an errors module for auth and an error class for invalid credentials

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,3 +1,4 @@
 export { authRouter } from './auth.router';
 export { AuthService } from './auth.service';
 export { JwtPayload } from './auth.types';
+export { InvalidCredentialsError } from './auth.errors';
