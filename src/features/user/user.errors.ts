13f64ddbb14f4940f271e0e4025f5de51c13743c
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-12-03 20:57:51 +0000

    feat(user): added errors for the user module

---
+++ src/features/user/user.errors.ts
@@ -0,0 +1,20 @@
+export class UserCreateError extends Error {
+  constructor() {
+    super('Error in creation of user');
+    this.name = 'UserCreateError';
+  }
+}
+
+export class UserRemoveError extends Error {
+  constructor() {
+    super('Error when removing a user');
+    this.name = 'UserRemoveError';
+  }
+}
+
+export class UserLoginError extends Error {
+  constructor() {
+    super('Error in user login');
+    this.name = 'UserLoginError';
+  }
+}
