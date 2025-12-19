44df5c47484f58aa5c04b84b20608a81dc6299df
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-12-19 00:45:19 +0000

    feat(user & security): Added a security module
    Added a security module with an interface for password hashing and injected it to UserService
    
    BREAKING CHANGE: Now UserService requires a PasswordHasher to be constructed

--- src/features/user/user.service.ts
+++ src/features/user/user.service.ts
@@ -8,11 +8,13 @@ import {
 } from '../user';
 import { AuthService, TokenSet } from '../auth';
 import { Result, Error, Ok } from '../../types';
+import { PasswordHasher } from '../../security';
 
 export class UserService {
   constructor(
     private userRepository: UserRepository,
-    private authService: AuthService
+    private authService: AuthService,
+    private passwordHasher: PasswordHasher
   ) {}
 
   async removeUser(userId: number): Promise<Result<User, UserRemoveError>> {
@@ -21,7 +23,7 @@ export class UserService {
   async loginUser(
     userData: UserData
   ): Promise<Result<TokenSet, UserLoginError>> {
-    const user = await this.userRepository.findUserByData(userData);
+    const user = await this.userRepository.findByData(userData);
     if (user) {
       return Ok(this.authService.generateTokens(user));
     } else {
@@ -31,7 +33,11 @@ export class UserService {
   async registerUser(
     userData: UserData
   ): Promise<Result<User, UserRegisterError>> {
-    const result = await this.userRepository.save(userData);
+    const hashedPassword = await this.passwordHasher.hash(userData.password);
+    const result = await this.userRepository.save({
+      ...userData,
+      password: hashedPassword,
+    });
     if (result.ok) {
       return Ok(result.value);
     } else {
