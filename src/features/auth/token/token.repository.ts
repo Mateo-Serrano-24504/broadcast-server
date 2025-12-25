a95445707d5260e8a05d44262a3961e98d979476
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-12-25 18:46:54 +0000

    feat(token): Added a TokenRepository interface

---
+++ src/features/auth/token/token.repository.ts
@@ -0,0 +1,17 @@
+import { Entity, Repository } from '../../../infraestructure';
+
+/**
+ * @template T Token entity to be stored
+ * @template TData Token data enough to create a token from it
+ * @template TValue Representative value of the token, often used as a key
+ *
+ * @description Repository for unique tokens. Tokens must be uniquely
+ * identified by a TValue item, often the token itself.
+ * */
+export interface TokenRepository<
+  T extends Entity<number>,
+  TData,
+  TValue,
+> extends Repository<T, TData, number> {
+  findByToken(token: TValue): Promise<null | T>;
+}
