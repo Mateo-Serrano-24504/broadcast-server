97cdd4f8757980186a972085c3cc1d5f72165ac2
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-12-25 18:54:08 +0000

    feat(token): Added a crypto token service

---
+++ src/features/auth/token/token.cryptoTokenService.ts
@@ -0,0 +1,39 @@
+import crypto from 'crypto';
+import { TokenService } from './token.service';
+import { RefreshTokenRepository } from './token.refreshTokenRepository';
+import { UserCredentials } from '../auth.types';
+import { assertOk } from '../../../types';
+import { assertHasValue } from '../../../utils/functions';
+
+export class CryptoTokenService implements TokenService<
+  UserCredentials,
+  string
+> {
+  constructor(
+    private readonly refreshTokenRepository: RefreshTokenRepository
+  ) {}
+
+  async generateToken(source: UserCredentials): Promise<string> {
+    const refreshTokenData = {
+      token: '',
+      userCredentials: {
+        role: source.role,
+        username: source.username,
+        id: source.id,
+      },
+    };
+    do {
+      refreshTokenData.token = crypto.randomBytes(64).toString('hex');
+    } while (
+      await this.refreshTokenRepository.findByToken(refreshTokenData.token)
+    );
+    assertOk(await this.refreshTokenRepository.save(refreshTokenData));
+    return refreshTokenData.token;
+  }
+
+  async validateToken(token: string): Promise<UserCredentials> {
+    const refresh = await this.refreshTokenRepository.findByToken(token);
+    assertHasValue(refresh);
+    return refresh.userCredentials;
+  }
+}
