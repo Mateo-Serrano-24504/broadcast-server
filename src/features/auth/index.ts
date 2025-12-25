e7996ea8352de6ef6bb7d74585552ad48457432c
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-12-25 18:36:38 +0000

    fix(auth): Added a missing import in index

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,3 +1,4 @@
 export { authRouter } from './auth.router';
 export { AuthService } from './auth.service';
 export { InvalidCredentialsError } from './auth.errors';
+export { TokenSet } from './auth.types';
