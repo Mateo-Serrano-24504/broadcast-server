5faae53b02a4784b9ede8e74a5e1b85f98be9d8b
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-12-25 18:42:26 +0000

    fix(db): Fixed schema.prisma
    Added a missing field in schema.prisma and fixed the userFromPrismaUser function

--- src/db/prisma.entities.ts
+++ src/db/prisma.entities.ts
@@ -1,6 +1,10 @@
 import { User } from '../features/user';
 import { User as PrismaUser } from '@prisma/client';
+import { userRoleFromString } from '../features/user/user.types';
+import { assertOk } from '../types';
 
 export function userFromPrismaUser(user: PrismaUser): User {
-  return new User(user.id, user.role, user.username, user.password);
+  const userRole = userRoleFromString(user.role);
+  assertOk(userRole);
+  return new User(user.id, userRole.value, user.username, user.password);
 }
