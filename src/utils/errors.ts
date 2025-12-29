09e556f5d3b43f477ee57fb7b00ea216d0bfb9f8
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-12-29 16:01:15 +0000

    chore(utils):
    Added a SelfNamedError class

---
+++ src/utils/errors.ts
@@ -0,0 +1,6 @@
+export class SelfNamedError extends Error {
+  protected constructor(message?: string) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export { SelfNamedError } from './errors';
